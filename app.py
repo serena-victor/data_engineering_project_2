9e6203209d2e04aaf98d5f4e6669cbd003b1545c
serena-victor <serena.victor@hotmail.fr>
2022-02-14 09:09:39 +0100

    connected toxicity model with webapp

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request
 import sys
 
+from detoxify import Detoxify
 
 app = Flask(__name__)
 
@@ -10,9 +11,16 @@ def root():
         return render_template('web_app.html')
     if request.method == 'POST':
         data = request.form.get('text_to_send')
-        result = data[::-1]
 
-        return render_template('result.html', result = result)
+        toxicity = Detoxify('original')
+        results = toxicity.predict(data)
+        
+        text_to_render = f'<br><strong>results for : "</strong>{data}"<br><br>'
+        for key in results :
+            key_percent = (results[key]*100).round(3)
+            text_to_render += f'&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{key} : {key_percent}%<br>'
+
+        return render_template('result.html', toxicity_results = text_to_render)
 
 
 app.run(host='localhost', port=5000)
